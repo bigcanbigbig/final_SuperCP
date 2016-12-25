334fee71d4100e50f109cf8d16971823a3b0101e
panda <elmas87532@gmail.com>
2016-12-25 22:33:45 +0800

    fix bugs of personal

--- public/assets/js/uploadimg.js
+++ public/assets/js/uploadimg.js
@@ -9,46 +9,7 @@ img2fire.controller("base64Ctrl", function($scope, $firebaseArray) {
   $scope.imgs = $firebaseArray(img);
 
   var _validFileExtensions = [".jpg", ".jpeg", ".bmp", ".gif", ".png"];
-  $scope.uploadFile = function() {
-    var sFileName = $("#nameImg").val();
-    if (sFileName.length > 0) {
-      var blnValid = false;
-      for (var j = 0; j < _validFileExtensions.length; j++) {
-        var sCurExtension = _validFileExtensions[j];
-        if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
-          blnValid = true;
-          var filesSelected = document.getElementById("nameImg").files;
-          if (filesSelected.length > 0) {
-            var fileToLoad = filesSelected[0];
-
-            var fileReader = new FileReader();
-
-            fileReader.onload = function(fileLoadedEvent) {
-              var textAreaFileContents = document.getElementById(
-                "textAreaFileContents"
-              );
-
-
-              $scope.imgs.$add({
-                date: Firebase.ServerValue.TIMESTAMP,
-                base64: fileLoadedEvent.target.result
-              });
-            };
-
-            fileReader.readAsDataURL(fileToLoad);
-          }
-          break;
-        }
-      }
-
-      if (!blnValid) {
-        alert('File is not valid');
-        return false;
-      }
-    }
-
-    return true;
-  }
+  
 
   $scope.deleteimg = function(imgid) {
     var r = confirm("Do you want to remove this image ?");
